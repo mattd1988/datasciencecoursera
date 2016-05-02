6c160824ac5e8f01b587a408395fc3e5c749158b
mattd1988 <mcpd110@gmail.com>
2016-05-01 22:44:54 -0400

    trying again

--- README.md
+++
@@ -1,4 +0,0 @@
-# datasciencecoursera
-Open the R script run_analysis.r using a text editor.
-Change the working directory to match your personal settings.
-Run the R script run_analysis.r.
--- run_analysis.R
+++ run_analysis.R
@@ -1,105 +1,33 @@
-#Preliminaries
-
-#Load packages.
-
-packages <- c("data.table", "reshape2")
-sapply(packages, require, character.only=TRUE, quietly=TRUE)
-#Set path.
-
-path <- getwd()
-path
-#Get the data
-
-#Download the file. Put it in the Data folder. This was already done on 2014-04-11; save time and don't evaluate again.
-
-url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-f <- "Dataset.zip"
-if (!file.exists(path)) {dir.create(path)}
-download.file(url, file.path(path, f))
-#Unzip the file. This was already done on 2014-04-11; save time and don't evaluate again.
-
-executable <- file.path("C:", "Program Files (x86)", "7-Zip", "7z.exe")
-parameters <- "x"
-cmd <- paste(paste0("\"", executable, "\""), parameters, paste0("\"", file.path(path, f), "\""))
-system(cmd)
-#The archive put the files in a folder named UCI HAR Dataset. Set this folder as the input path. List the files here.
-
-pathIn <- file.path(path, "UCI HAR Dataset")
-list.files(pathIn, recursive=TRUE)
-
-dtSubjectTrain <- fread(file.path(pathIn, "train", "subject_train.txt"))
-dtSubjectTest  <- fread(file.path(pathIn, "test" , "subject_test.txt" ))
-
-dtActivityTrain <- fread(file.path(pathIn, "train", "Y_train.txt"))
-dtActivityTest  <- fread(file.path(pathIn, "test" , "Y_test.txt" ))
-
-fileToDataTable <- function (f) {
-        df <- read.table(f)
-        dt <- data.table(df)
-}
-dtTrain <- fileToDataTable(file.path(pathIn, "train", "X_train.txt"))
-dtTest  <- fileToDataTable(file.path(pathIn, "test" , "X_test.txt" ))
-
-dtSubject <- rbind(dtSubjectTrain, dtSubjectTest)
-setnames(dtSubject, "V1", "subject")
-dtActivity <- rbind(dtActivityTrain, dtActivityTest)
-setnames(dtActivity, "V1", "activityNum")
-dt <- rbind(dtTrain, dtTest)
-
-dtSubject <- cbind(dtSubject, dtActivity)
-dt <- cbind(dtSubject, dt)
-
-setkey(dt, subject, activityNum)
-
-dtFeatures <- fread(file.path(pathIn, "features.txt"))
-setnames(dtFeatures, names(dtFeatures), c("featureNum", "featureName"))
-
-dtFeatures <- dtFeatures[grepl("mean\\(\\)|std\\(\\)", featureName)]
-
-dtFeatures$featureCode <- dtFeatures[, paste0("V", featureNum)]
-head(dtFeatures)
-dtFeatures$featureCode
-
-select <- c(key(dt), dtFeatures$featureCode)
-dt <- dt[, select, with=FALSE]
-
-dtActivityNames <- fread(file.path(pathIn, "activity_labels.txt"))
-setnames(dtActivityNames, names(dtActivityNames), c("activityNum", "activityName"))
-
-dt <- merge(dt, dtActivityNames, by="activityNum", all.x=TRUE)
-
-setkey(dt, subject, activityNum, activityName)
-
-dt <- data.table(melt(dt, key(dt), variable.name="featureCode"))
-
-dt <- merge(dt, dtFeatures[, list(featureNum, featureCode, featureName)], by="featureCode", all.x=TRUE)
-
-dt$activity <- factor(dt$activityName)
-dt$feature <- factor(dt$featureName)
-
-grepthis <- function (regex) {
-        grepl(regex, dt$feature)
-}
-n <- 2
-y <- matrix(seq(1, n), nrow=n)
-x <- matrix(c(grepthis("^t"), grepthis("^f")), ncol=nrow(y))
-dt$featDomain <- factor(x %*% y, labels=c("Time", "Freq"))
-x <- matrix(c(grepthis("Acc"), grepthis("Gyro")), ncol=nrow(y))
-dt$featInstrument <- factor(x %*% y, labels=c("Accelerometer", "Gyroscope"))
-x <- matrix(c(grepthis("BodyAcc"), grepthis("GravityAcc")), ncol=nrow(y))
-dt$featAcceleration <- factor(x %*% y, labels=c(NA, "Body", "Gravity"))
-x <- matrix(c(grepthis("mean()"), grepthis("std()")), ncol=nrow(y))
-dt$featVariable <- factor(x %*% y, labels=c("Mean", "SD"))
-dt$featJerk <- factor(grepthis("Jerk"), labels=c(NA, "Jerk"))
-dt$featMagnitude <- factor(grepthis("Mag"), labels=c(NA, "Magnitude"))
-n <- 3
-y <- matrix(seq(1, n), nrow=n)
-x <- matrix(c(grepthis("-X"), grepthis("-Y"), grepthis("-Z")), ncol=nrow(y))
-dt$featAxis <- factor(x %*% y, labels=c(NA, "X", "Y", "Z"))
-
-r1 <- nrow(dt[, .N, by=c("feature")])
-r2 <- nrow(dt[, .N, by=c("featDomain", "featAcceleration", "featInstrument", "featJerk", "featMagnitude", "featVariable", "featAxis")])
-r1 == r2
-
-setkey(dt, subject, activity, featDomain, featAcceleration, featInstrument, featJerk, featMagnitude, featVariable, featAxis)
-dtTidy <- dt[, list(count = .N, average = mean(value)), by=key(dt)]
+if(!file.exists("./data")){dir.create("./data")}
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileURL, destfile = "./data/wearable.zip")
+unzip(zipfile = "./data/wearable.zip")
+path_rf <- file.path("./data" , "UCI HAR Dataset")
+files<-list.files(path_rf, recursive=TRUE)
+XTest<- read.table("UCI HAR Dataset/test/X_test.txt")
+YTest<- read.table("UCI HAR Dataset/test/Y_test.txt")
+SubjectTest <-read.table("UCI HAR Dataset/test/subject_test.txt")
+XTrain<- read.table("UCI HAR Dataset/train/X_train.txt")
+YTrain<- read.table("UCI HAR Dataset/train/Y_train.txt")
+SubjectTrain <-read.table("UCI HAR Dataset/train/subject_train.txt")
+features<-read.table("UCI HAR Dataset/features.txt")
+activity<-read.table("UCI HAR Dataset/activity_labels.txt")
+dataSubject <- rbind(SubjectTrain, SubjectTest)
+dataActivity<- rbind(XTrain, XTest)
+dataFeatures<- rbind(YTrain, YTest)
+names(dataSubject)<-c("subject")
+names(dataActivity)<- c("activity")
+names(dataFeatures)<- dataFeatures$V2
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+index<-grep("mean\\(\\)|std\\(\\)", features[,2])
+dataActivity<-dataActivity[,index]
+dataFeatures[,1]<-activity[dataFeatures[,1],2]
+names<-features[index,2]
+names(dataActivity)<-names
+names(dataSubject)<-"SubjectID"
+names(dataFeatures)<-"Activity"
+CleanedData<-cbind(dataSubject, dataFeatures, dataActivity)
+CleanedDataTable<-data.table(CleanedData)
+TidyData <- CleanedDataTable[, lapply(.SD, mean), by = 'SubjectID,Activity']
+write.table(TidyData, file = "Tidy.txt", row.names = FALSE)
