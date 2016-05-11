9140e74e2c0ad9a424ca2d3a8b8b882a08439d4a
mattd1988 <mcpd110@gmail.com>
2016-05-11 16:01:38 -0400

    Delete Pollutantmean.r

--- Pollutantmean.r
+++
@@ -1,9 +0,0 @@
-pollutantmean <- function(specdata, pollutant, id = 1:332) {
-        specdata <- "/Users/matthew_dorst/Desktop/Coursera/specdata/"
-        data=numeric()
-        for(i in id)
-                {newRead=read.csv(paste(specdata,formatC(i,width=3,flag="0"),".csv",sep=""))
-		data=c(data,newRead[[pollutant]])
-		}
-        return(print(mean(data,na.rm=TRUE),digits=4))
-        }
