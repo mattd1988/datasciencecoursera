7432693df4d188aa36251c0015599e20a8e8ba7e
mattd1988 <mcpd110@gmail.com>
2016-05-11 16:00:13 -0400

    Delete complete.r

--- complete.r
+++
@@ -1,9 +0,0 @@
-complete <- function(specdata, id = 1:332) {
-        specdata <- "/Users/matthew_dorst/Desktop/Coursera/specdata/"
-        nobs = numeric()
-        for(i in id)
-                {newRead=read.csv(paste(specdata,formatC(i,width=3,flag="0"),".csv",sep=""))
-		nobs=c(nobs,sum(complete.cases(newRead)))
-		}
-	return(data.frame(id,nobs))
-}
